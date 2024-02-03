a6834e244b8fe6902a48f35a8334dff8b661466e
Maxime Bonin <maximebonin@outlook.com>
2024-02-03 09:09:07 -0500

    adding license copyright in code output

--- main.py
+++ main.py
@@ -3,8 +3,16 @@ import frontmatter
 import pandas as pd
 import yaml
 
+LICENSE_TXT = '''
 
-def main(dir_path: str, config: dict) -> int:
+    metadata_to_csv  Copyright (C) 2024  Maxime Bonin
+    This program comes with ABSOLUTELY NO WARRANTY; for details type `show w'.
+    This is free software, and you are welcome to redistribute it
+    under certain conditions; type `show c' for details.
+
+'''
+
+def main(dir_path: str, config: dict) -> None:
     directory = os.fsencode(dir_path)
 
     files = []
@@ -26,24 +34,24 @@ def main(dir_path: str, config: dict) -> int:
     df = pd.DataFrame(metadata_list)
     df.to_csv(config["csv_file_name"], index=False)
 
-    return 0
-
 
 if __name__ == "__main__":
+    print(LICENSE_TXT)
+
     try:
         files_directory_name = sys.argv[1]
     except:
         print("No arguments were given")
-        print("file directory name is expected\r")
+        print("file directory(within current directory) name is expected\r")
         exit(1)
 
-    with open("config.yml", "r") as config_file:
-        config = yaml.load(config_file, Loader=yaml.FullLoader)
-        print("read config file successfully\r")
-    return_code = main(f"{os.getcwd()}/{files_directory_name}/", config=config)
-    if return_code == 0:
+    try:
+        with open("config.yml", "r") as config_file:
+            config = yaml.load(config_file, Loader=yaml.FullLoader)
+            print("read config file successfully\r")
+        main(f"{os.getcwd()}/{files_directory_name}/", config=config)
         print(f'find your CSV metada file at: {os.getcwd()}/{config["csv_file_name"]}')
-    else:
+    except:
         print("an error occured, so sorry")
-    
-    exit(return_code)
+
+    exit(0)
